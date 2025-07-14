b4967f2bf44b6202c59fb797e44f5fa9e0db1ba6
Anuj-Parihar <anujparihar0001@gmail.com>
2025-07-15 01:39:40 +0530

    commit

--- app/page.js
+++ app/page.js
@@ -21,3 +21,34 @@ export default function Home() {
     </div>
   );
 }
+
+
+// "use client";
+
+// import dynamic from 'next/dynamic';
+// import AboutSection from "./components/homepage/about";
+// import ContactSection from "./components/homepage/contact";
+// import Education from "./components/homepage/education";
+// import Experience from "./components/homepage/experience";
+// import Skills from "./components/homepage/skills";
+// import Projects from "./components/homepage/projects";
+
+// // Dynamically import HeroSection with SSR disabled
+// const HeroSection = dynamic(
+//   () => import('./components/homepage/hero-section'),
+//   { ssr: false }
+// );
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <HeroSection />
+//       <AboutSection />
+//       <Experience />
+//       <Skills />
+//       <Projects />
+//       <Education />
+//       <ContactSection />
+//     </div>
+//   );
+// }
