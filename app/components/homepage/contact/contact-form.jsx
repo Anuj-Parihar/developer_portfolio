065ef2e56d6b8f8ab25e8ce4b2838759e2e30d88
Anuj-Parihar <anujparihar0001@gmail.com>
2025-12-31 00:12:29 +0530

    telegram bot changes

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -36,10 +36,7 @@ function ContactForm() {
 
     try {
       setIsLoading(true);
-      const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
-        userInput
-      );
+      const res = await axios.post('/api/contact', userInput);
 
       toast.success("Message sent successfully!");
       setUserInput({
@@ -47,6 +44,7 @@ function ContactForm() {
         email: "",
         message: "",
       });
+    
     } catch (error) {
       toast.error(error?.response?.data?.message);
     } finally {
