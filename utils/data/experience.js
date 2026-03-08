64a45d31911d3daa84d39addd9b602da24ac3d11
Anuj-Parihar <anujparihar0001@gmail.com>
2026-03-09 01:29:05 +0530

    add exp

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,21 @@
 export const experiences = [
-  // {
-  //   id: 1,
-  //   title: 'Software Developer Intern (Remote)',
-  //   company: "Exzellent GmbH",
-  //   duration: "(Jun 2025 - Sep 2025)"
-  // },
   {
     id: 1,
+    title: 'AI Automation Intern',
+    company: "Advaiya Solutions Inc.",
+    duration: "(Fab 2026 - present)"
+  },
+  {
+    id: 2,
+    title: 'Software Developer Intern (Remote)',
+    company: "Exzellent GmbH",
+    duration: "(Jun 2025 - Sep 2025)"
+  },
+  {
+    id: 3,
     title: "MERN Stack Developer Internship (On-Site)",
     company: "Marvik Solutions Private Limited",
     duration: "(Jun 2024 - July 2024)"
   },
-  {
-    id: 2,
-    title: "Founder of ISDC Club",
-    company: "Mohanlal Sukhadia University (MLSU), Udaipur",
-    duration: "(July 2021 - June 2024)"
-  }
+  
 ]
